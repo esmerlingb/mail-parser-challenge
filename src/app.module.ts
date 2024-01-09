75e69fb3304eeb299b126aa48e40cfdbcb32e888
Angel Gonzalez <angel.egb@outlook.com>
2024-01-09 11:13:12 -0400

    Change port by config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import appConfig from './config/app.config'
+import { AppErrorModule } from './errors/app-error.module'
 import { MailEventsModule } from './mail-events/events.module'
 import { MailParserModule } from './mail-parser/mail-parser.module'
-import { AppErrorModule } from './errors/app-error.module'
 
 @Module({
-  imports: [MailEventsModule, MailParserModule, AppErrorModule]
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig]
+    }),
+    MailEventsModule,
+    MailParserModule,
+    AppErrorModule
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,6 +12,8 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(3000)
+  const configService = app.get<ConfigService>(ConfigService)
+
+  await app.listen(configService.get('port') as string)
 }
 bootstrap()
